3a311069144d5c93e37d6b0dc015754fb1b8914a
suryarameshkumar23123-pixel <suryarameshkumar23123@gmail.com>
2026-04-21 21:43:47 +0530

    Update text output element from 'result' to 'outputText'

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function simplifyText() {
     let input = document.getElementById("inputText").value;
-    let result = "Simplified:\n" + input.substring(0, 150);
-    document.getElementById("result").innerText = result;
+    document.getElementById("outputText").innerText =
+        "Simplified:\n" + input.substring(0,150);
 }
 
 function summarizeText() {
     let input = document.getElementById("inputText").value;
-    let result = "Summary:\n" + input.substring(0, 80) + "...";
-    document.getElementById("result").innerText = result;
+    document.getElementById("outputText").innerText =
+        "Summary:\n" + input.substring(0,80) + "...";
 }
 
 function clearText() {
     document.getElementById("inputText").value = "";
-    document.getElementById("result").innerText = "Your result will appear here...";
+    document.getElementById("outputText").innerText =
+        "Your result will appear here...";
 }
